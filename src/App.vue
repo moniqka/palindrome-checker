69d287972735c8a79935eb283e38d171ee2c6683
Monika Kowalska <m.kowalska@futureit.agency>
2020-09-11 20:08:55 +0200

    add basic components and style library

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Checker/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Checker from "./components/Checker.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Checker
   }
 };
 </script>
 
 <style lang="scss">
+@import "@/scss/_library.scss";
+//////////////// MAIN RULES ////////////////
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  outline: none;
+}
+html {
+  height: 100%;
+}
+body {
+  font-weight: normal;
+  min-height: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  background: $body-bg;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: $body-bg;
   margin-top: 60px;
+  font-family: $basefont;
+  text-align: center;
 }
 </style>
---
+++ src/components/Checker.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <header>
+      <titile>
+        Palindrome Checker
+      </titile>
+      <p>
+        A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward.
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Checker",
+  }
+</script>
+
+<style lang="scss">
+@import "@/scss/_library.scss";
+</style>
